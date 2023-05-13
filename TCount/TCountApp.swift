a98e712161689b3eee284e08fe4f5f2754fbf116
박준현 <23wdwad@gmail.com>
2023-05-14 00:54:25 +0900

    Initial Commit

---
+++ TCount/TCountApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCountApp.swift
+//  TCount
+//
+//  Created by 4rNe5 on 2023/05/14.
+//
+
+import SwiftUI
+
+@main
+struct TCountApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
