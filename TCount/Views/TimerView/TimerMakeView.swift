5610e72d8b79a48c075d4a2679845d0d6bd695bb
박준현 <23wdwad@gmail.com>
2023-05-26 09:50:45 +0900

    Update TimerMakeView.swift
    Change in Progress...

--- TCount/Views/TimerView/TimerMakeView.swift
+++ TCount/Views/TimerView/TimerMakeView.swift
@@ -7,16 +7,32 @@
 import SwiftUI
 
 struct TimerMakeView: View {
-    @State var date = Date()
+    @State private var goals = ["목표 1", "목표 2", "목표 3"]
+    @State private var tasks = ["할 일 1", "할 일 2", "할 일 3"]
+
     var body: some View {
         Text("새로운 타이머 만들기")
-        
+            .font(.title)
+            .bold()
+            .offset(x: 0, y: -150)
+            .padding()
+        List {
+            ForEach(goals, id: \.self) { goal in
+                Section(header: Text(goal)) {
+                    ForEach(tasks) { task in
+                        TextField("할 일", text: $task)
+                    }
+                }
+            }
+        }
     }
-    
+}
+
+
+
     
     struct TimerMakeView_Previews: PreviewProvider {
         static var previews: some View {
             ContentView()
         }
     }
-}
